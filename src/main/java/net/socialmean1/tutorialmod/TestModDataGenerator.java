30c80e5d9e9404ee7b2527c90ed3ba389983054e
CreativeRealmGames <creativerealmgames@gmail.com>
2025-11-30 20:03:31 -0600

    EXTREMELY IMPORTANT CHANGE

---
+++ src/main/java/net/socialmean1/tutorialmod/TestModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.socialmean1.tutorialmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TestModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
