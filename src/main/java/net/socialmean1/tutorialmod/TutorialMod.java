bd946176925afe175486262c9a93c6f0925ca4cd
CreativeRealmGames <creativerealmgames@gmail.com>
2025-11-30 21:11:41 -0600

    EXTREMELY IMPORTANT CHANGE

--- src/main/java/net/socialmean1/tutorialmod/TutorialMod.java
+++ src/main/java/net/socialmean1/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.socialmean1.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.socialmean1.tutorialmod.item.ModItemGroups;
+import net.socialmean1.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItemGroups.registerItemGroups();
+        ModItems.registerModItems();
 	}
 }
